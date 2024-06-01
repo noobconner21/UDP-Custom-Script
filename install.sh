c5801bd2566e0bce5190e5c876da8528420fcf8c
noobconner21 <kenwayshay21@gmail.com>
2024-06-01 16:44:12 +0530

    update version 1.5

---
+++ install.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+apt update -y
+apt upgrade -y
+apt install locat -y
+apt install figlet -y
+apt install neofetch -y
+cd
+rm -rf /root/udp
+mkdir -p /root/udp
+
+# banner
+
+echo -e "          ░█▀▀▀█ ░█▀▀▀█ ░█─── ─█▀▀█ ░█▀▀█ 　 ░█─░█ ░█▀▀▄ ░█▀▀█ " | lolcat
+echo -e "          ─▀▀▀▄▄ ─▀▀▀▄▄ ░█─── ░█▄▄█ ░█▀▀▄ 　 ░█─░█ ░█─░█ ░█▄▄█ " | lolcat
+echo -e "          ░█▄▄▄█ ░█▄▄▄█ ░█▄▄█ ░█─░█ ░█▄▄█ 　 ─▀▄▄▀ ░█▄▄▀ ░█─── " | lolcat
+echo ""
+echo ""
+echo ""
+sleep 5
+# change to time GMT+7
+echo "change to time GMT+7"
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# install udp-custom
+echo downloading udp-custom
+wget "https://github.com/Haris131/UDP-Custom-Script/raw/main/udp-custom-linux-amd64" -O /root/udp/udp-custom
+chmod +x /root/udp/udp-custom
+
+echo downloading default config
+wget "https://raw.githubusercontent.com/Haris131/UDP-Custom-Script/main/config.json" -O /root/udp/config.json
+chmod 644 /root/udp/config.json
+
+if [ -z "$1" ]; then
+cat <<EOF > /etc/systemd/system/udp-custom.service
+[Unit]
+Description=UDP Custom by ePro Dev. Team and modify by sslablk
+
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/udp-custom server
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2s
+
+[Install]
+WantedBy=default.target
+EOF
+else
+cat <<EOF > /etc/systemd/system/udp-custom.service
+[Unit]
+Description=UDP Custom by ePro Dev. Team and modify by sslablk
+
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/udp-custom server -exclude $1
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2s
+
+[Install]
+WantedBy=default.target
+EOF
+fi
+
+clear
+echo '    Install Custom UDP Manager   ' | lolcat
+
+echo ''
+echo ''
+echo ''
+sleep 5
+cd $HOME
+mkdir /etc/Sslablk
+cd /etc/Sslablk
+wget https://github.com/noobconner21/UDP-Custom-Script/raw/main/system.zip
+unzip system
+cd /etc/Sslablk/system
+mv menu /usr/local/bin
+cd /etc/Sslablk/system
+chmod +x ChangeUser.sh
+chmod +x Adduser.sh
+chmod +x DelUser.sh
+chmod +x Userlist.sh
+chmod +x RemoveScript.sh
+chmod +x torrent.sh
+cd /usr/local/bin
+chmod +x menu
+cd /etc/Sslablk
+rm system.zip
+
+
+clear
+echo 'UDP Install Script By Project SSLAB LK Dev.Team'
+echo 'UDP Custom By ePro Dev. Team'
+echo ''
+echo ''
+echo ' Support US'
+echo "Github/noobconner21"
+echo "Telegram/SSLAB LK"
+sleep 5
+
+echo start service udp-custom
+systemctl start udp-custom &>/dev/null
+
+echo enable service udp-custom
+systemctl enable udp-custom &>/dev/null
+
+echo reboot
+reboot
